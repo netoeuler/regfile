6ba51db9dec10db331d100dc3539d67a84f5ed1e
Euler Neto <eulerneto.ngt@gmail.com>
2020-12-15 17:29:46 -0300

    refactoring base block

--- regfile.py
+++ regfile.py
@@ -11,70 +11,23 @@ base_block = regfile.read(4096)
 if base_block[0:4].decode('utf-8') != 'regf':
 	die('Wrong magic number')
 
-field_value = ''
-for i in range(7,3,-1):
-	field_value+= str(hex(base_block[i])).replace('0x','')
-print('Primary sequence number: '+str(int('0x'+field_value,0)))
+#Array with elements that contains [field_name, offset, length]
+field_array = [['Primary sequence number',4,4], ['Secondary sequence number',8,4], ['Last written timestamp',12,8], 
+	['Major version',20,4], ['Minor version',24,4], ['File type',28,4], ['File format',32,4], ['Root cell offset',36,4],
+	['Hive bins data size',40,4], ['Clustering factor',44,4], ['Filename',48,64], ['Checksum',508,4], ['Boot type',4088,4],
+	['Boot recover',4092,4]]
 
-field_value = ''
-for i in range(11,7,-1):
-	field_value+= str(hex(base_block[i])).replace('0x','')
-print('Secondary sequence number: '+str(int('0x'+field_value,0)))
+for f in field_array:
+	field_name = f[0]
+	offset = f[1]
+	length = f[2]
 
-field_value = ''
-for i in range(19,11,-1):
-	field_value+= str(hex(base_block[i])).replace('0x','')
-print('Last written timestamp: '+str(int('0x'+field_value,0)))
-#print('Last written timestamp: '+str(datetime.fromtimestamp(int('0x'+field_value,0))))
+	if field_name == 'Filename':
+		print('Filename: '+base_block[48:112].decode('utf-16'))
+		continue
 
-field_value = ''
-for i in range(23,19,-1):
-	field_value+= str(hex(base_block[i])).replace('0x','')
-print('Major version: '+str(int('0x'+field_value,0)))
-
-field_value = ''
-for i in range(27,23,-1):
-	field_value+= str(hex(base_block[i])).replace('0x','')
-print('Minor version: '+str(int('0x'+field_value,0)))
-
-field_value = ''
-for i in range(31,27,-1):
-	field_value+= str(hex(base_block[i])).replace('0x','')
-print('File type: '+str(int('0x'+field_value,0)))
-
-field_value = ''
-for i in range(35,31,-1):
-	field_value+= str(hex(base_block[i])).replace('0x','')
-print('File format: '+str(int('0x'+field_value,0)))
-
-field_value = ''
-for i in range(39,35,-1):
-	field_value+= str(hex(base_block[i])).replace('0x','')
-print('Root cell offset: '+str(int('0x'+field_value,0)))
-
-field_value = ''
-for i in range(43,39,-1):
-	field_value+= str(hex(base_block[i])).replace('0x','')
-print('Hive bins data size: '+str(int('0x'+field_value,0)))
-
-field_value = ''
-for i in range(47,43,-1):
-	field_value+= str(hex(base_block[i])).replace('0x','')
-print('Clustering factor: '+str(int('0x'+field_value,0)))
-
-print('Filename: '+base_block[48:112].decode('utf-16'))
-
-field_value = ''
-for i in range(511,507,-1):
-	field_value+= str(hex(base_block[i])).replace('0x','')
-print('Checksum: '+str(int('0x'+field_value,0)))
-
-field_value = ''
-for i in range(4091,4087,-1):
-	field_value+= str(hex(base_block[i])).replace('0x','')
-print('Boot type: '+str(int('0x'+field_value,0)))
-
-field_value = ''
-for i in range(4095,4091,-1):
-	field_value+= str(hex(base_block[i])).replace('0x','')
-print('Boot recover: '+str(int('0x'+field_value,0)))
+	field_value = ''
+	print(offset+length-1, str(offset-1))
+	for i in range(offset+length-1, offset-1, -1):
+		field_value+= str(hex(base_block[i])).replace('0x','')
+	print(field_name+': '+str(int('0x'+field_value,0)))
